5461ed9e82da77d34772e72aa611c3cb3cfaffe9
Kirill495 <k495@mail.ru>
2024-10-20 14:53:17 +0300

    refactor: соответствие "ROMAN_NUMBERS" стало static

--- RomanToInteger/src/Solution2.java
+++ RomanToInteger/src/Solution2.java
@@ -2,7 +2,7 @@ import java.util.Map;
 
 public class Solution2 {
 
-    private final Map<Character, Integer> ROMAN_NUMBERS =
+    private static final Map<Character, Integer> ROMAN_NUMBERS =
             Map.of('I', 1, 'V', 5, 'X', 10, 'L', 50, 'C', 100, 'D', 500, 'M', 1000);
 
     public int romanToInt(String s) {

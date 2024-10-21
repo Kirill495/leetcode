a9b20c0b9a78816e9d2119570f00f03e8ec9d64e
Kirill495 <k495@mail.ru>
2024-10-21 08:31:21 +0300

    test:  разработать тесты для LinkedListCycle

--- LinkedListCycle/test/SolutionTest.java
+++ LinkedListCycle/test/SolutionTest.java
@@ -1,18 +1,57 @@
 import listNode.ListNode;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SolutionTest {
 
     Solution solution = new Solution();
     @Test
     public void listHasCycle() {
-        ListNode node1 = new ListNode(0);
-        node1.next = new ListNode(1);
-        node1.next.next = new ListNode(2);
-        node1.next.next.next = node1;
+        ListNode node = prepareList(new int[]{3, 2, 0, -4}, 1);
 
-        assertTrue(solution.HasCycle(node1));
+        assertTrue(solution.hasCycle(node));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "testsource/input.csv", delimiter = ';')
+    public void listsAreCycled(String headStr, int pos, boolean isCycled) {
+        int[] head = Arrays.stream(headStr.split(",")).mapToInt(Integer::valueOf).toArray();
+        ListNode node = prepareList(head, pos);
+        if (isCycled) {
+            assertTrue(solution.hasCycle(node));
+        } else {
+            assertFalse(solution.hasCycle(node));
+        }
+    }
+
+    private ListNode prepareList(int[] head, int pos) {
+
+        ListNode cycledNode = null, headNode = null, newNode = null, prevNode = null;
+
+        for (int i = 0; i < head.length; i++) {
+            newNode = new ListNode(head[i]);
+
+            if (i == pos) {
+                cycledNode = newNode;
+            }
+            if (i == 0) {
+                headNode = newNode;
+            } else {
+                if (prevNode != null) {
+                    prevNode.next = newNode;
+                }
+            }
+            prevNode = newNode;
+        }
+        if (cycledNode != null) {
+            newNode.next = cycledNode;
+        }
+        return headNode;
     }
 }

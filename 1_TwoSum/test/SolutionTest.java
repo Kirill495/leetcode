311d792e212d7602c06ff709a031152258a5bc8f
Kirill495 <k495@mail.ru>
2026-01-28 08:20:27 +0300

    49 Group anagram

--- 1_TwoSum/test/SolutionTest.java
+++ 1_TwoSum/test/SolutionTest.java
@@ -5,9 +5,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class SolutionTest {
     @Test
     public void testSolution() {
-        assertArrayEquals(new int[] {0,1}, new Solution().toSum(new int[] {2, 7, 11, 15}, 9));
-        assertArrayEquals(new int[] {1,2}, new Solution().toSum(new int[] {3, 2, 4}, 6));
-        assertArrayEquals(new int[] {0,1}, new Solution().toSum(new int[] {3, 3}, 6));
+        Solution3 s = new Solution3();
+        assertArrayEquals(new int[] {0,1}, s.twoSum(new int[] {2, 7, 11, 15}, 9));
+        assertArrayEquals(new int[] {1,2}, s.twoSum(new int[] {3, 2, 4}, 6));
+        assertArrayEquals(new int[] {0,1}, s.twoSum(new int[] {3, 3}, 6));
     }
 
 }
